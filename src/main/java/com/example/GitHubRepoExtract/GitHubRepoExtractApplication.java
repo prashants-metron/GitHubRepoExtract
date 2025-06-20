ef81174e1bf2af234b5957faf5d09e8c76a1b440
prashants-metron <prashant.sahu@metronlabs.com>
2025-06-20 14:45:16 +0530

    fc

---
+++ src/main/java/com/example/GitHubRepoExtract/GitHubRepoExtractApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitHubRepoExtract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepoExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepoExtractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitHubRepoExtract/GitHubRepoExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubRepoExtract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoExtractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
